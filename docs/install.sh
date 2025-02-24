d7d7ed2a36f56516dc05a6b37e23d39dd46097a9
Vivian Elisabeth H <36878099+SparkleDEV@users.noreply.github.com>
2025-02-24 02:13:02 +0100

    Added hyprland config

--- docs/install.sh
+++ docs/install.sh
@@ -2,66 +2,60 @@
 
 echo "!!!DISCLAIMER!!!
 I made this dotfile installer JUST FOR MYSELF.
-You are totally free to use it, however I don't take responsibility if that script does something you don't want it to do.
+It does things like setting up my hyprland config, installing various packages, changing my shell, etc.
+You are totally free to use it, however I don't take responsibility if that script does something you don't want it to do, and I HIGHLY ADVISE AGAINST IT.
 So READ THE SCRIPT before you execute."
 
 read -p "Do you want to continue? (y/N) " c_execute
 c_execute=${c_execute:-N}
 
 case "$c_execute" in
-	y|Y )
-		;;
-	* )
-		exit 0
-		;;
+    y|Y )
+    ;;
+    * )
+        exit 0
+    ;;
 esac
 
 DOTFILES_DIR=$HOME/.dotfiles
 
 if [ -d "$DOTFILES_DIR" ]; then
-	read -p "$DOTFILES_DIR already exists, do you want to override it? (y/N) " choice
-	choice=${choice:-N}
-
-	case "$choice" in
-		y|Y )
-			echo "$DOTFILES_DIR will be overwritten."
-			rm -rf $DOTFILES_DIR
-			;;
-		* )
-			echo "User did not consent for $DOTFILES_DIR to be overwritten, aborting."
-			exit 0
-			;;
-	esac
+    read -p "$DOTFILES_DIR already exists, do you want to override it? (y/N) " choice
+    choice=${choice:-N}
+    
+    case "$choice" in
+        y|Y )
+            echo "$DOTFILES_DIR will be overwritten."
+            rm -rf $DOTFILES_DIR
+        ;;
+        * )
+            echo "User did not consent for $DOTFILES_DIR to be overwritten, aborting."
+            exit 0
+        ;;
+    esac
 fi
 
 sudo -v
 
-sudo bash <<EOF
-	echo "Installing packages"
-	pacman -Syy
-	pacman -S vim fish fastfetch hyfetch nodejs tree git stow nerd-fonts reflector --noconfirm --needed
-	echo "Setting user shell to fish"
-	chsh -s /bin/fish $USER
-EOF
-
-sudo -v
-
 if ! command -v yay &> /dev/null; then
-	echo "yay was not found on system, installing..."
-	TEMP_DIR=`mktemp -d`
-
-	git clone https://aur.archlinux.org/yay.git $TEMP_DIR/yay
-	cd $TEMP_DIR/yay
-
-	makepkg -si --noconfirm
+    echo "yay was not found on system, installing..."
+    TEMP_DIR=`mktemp -d`
+    
+    git clone https://aur.archlinux.org/yay.git $TEMP_DIR/yay
+    cd $TEMP_DIR/yay
+    
+    makepkg -si --noconfirm
 fi
 
 sudo -v
 
 echo "Installing AUR packages"
-yay -S arrpc pokeget oh-my-posh --noconfirm --needed
+yay -S vim fish fastfetch hyfetch nodejs tree git stow nerd-fonts reflector arrpc pokeget oh-my-posh hyprland hyprshot hyprlock hyprpaper hypridle brightnessctl playerctl pavucontrol nm-connection-editor uswm sddm waybar ttf-font-awesome swaync libnotify kitty wofi nautilus --noconfirm --needed
 
 sudo -v
+
+echo "Setting user shell to fish"
+chsh -s /bin/fish $USER
 
 echo "arrpc systemd fix..."
 
